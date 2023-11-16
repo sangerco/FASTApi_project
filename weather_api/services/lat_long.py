5455a34a60afbff9a2185f0ab799ec85aa3688ec
Dan Sanger <sangerco@yahoo.com>
2023-11-16 14:51:14 -0700

    removed api key from lat_long

--- weather_api/services/lat_long.py
+++ weather_api/services/lat_long.py
@@ -1,8 +1,9 @@
 from typing import Optional
+from secret import api_key
 
 
 def get_lat_long(city: str, state: Optional[str], country: str = "US") -> dict:
     q = f"{city}, {state}, {country}"
-    key = "c3478b985b67412db8f6ee7de66c7228"
+    key = api_key
 
     url = f"https://api.opencagedata.com/geocode/v1/json?={q}"
